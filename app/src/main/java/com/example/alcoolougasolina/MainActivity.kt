4e081a38dbc326e441c91f4a65b582f9044d8113
IsraelCoeio <israelcoelhocunhaccc@gmail.com>
2024-03-27 21:08:36 -0300

    Minimum Requirements

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -6,25 +6,47 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     var percentual:Double = 0.7
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putDouble("percentual",percentual)
+        super.onSaveInstanceState(outState)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState != null) {
+            percentual=savedInstanceState.getDouble("percentual")
+        }
         Log.i("PDM24","No onCreate, $percentual")
-
-        percentual=0.7
         val btCalc: Button = findViewById(R.id.btCalcular)
         val textMsg:TextView= findViewById(R.id.textMsg)
+        val alcPrice: EditText = findViewById(R.id.edAlcool)
+        val gasPrice: EditText = findViewById(R.id.edGasolina)
         //btCalc.setBackgroundColor(Color.CYAN)
         btCalc.setOnClickListener(View.OnClickListener {
             //código do evento
-            percentual=0.75
-            textMsg.text="Já tenho o percentual"
+            val proportion = alcPrice.text.toString().toDouble()/gasPrice.text.toString().toDouble()
+            if(proportion<percentual){
+                textMsg.text="Abasteça com Álcool."
+            } else {
+                textMsg.text = "Abasteça com Gasolina."
+            }
             Log.d("PDM24","No btCalcular, $percentual")
         })
+    }
+
+    fun onClickSwitch(v:View){
+        val s : Switch = v as Switch
+        if (s.isChecked){
+            percentual = 0.75
+        } else {
+            percentual = 0.7
+        }
     }
 override fun onResume(){
     super.onResume()
